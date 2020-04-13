de73eb4283573ebf87408e5df890f40c59d4bb51
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2020-04-13 15:45:00 +0530

    refactor code to airbnb js for standardised style

--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,55 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [];
   let animalData;
   let animalArray = [];
-  const randomAnimalImagesSite = "https://www.randomlists.com";
-  function status(response) {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(new Error(response.statusText));
-    }
-  }
+  const randomAnimalImagesSite = 'https://www.randomlists.com';
+  const isFlippedClassname = 'is-flipped';
+  const blankImagePath = 'images/blank.png';
+  const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  const remainingDisplay = document.querySelector('#remaining');
+  const refreshButton = document.getElementById('refresh-container');
+  const restoreButton = document.getElementById('restore-container');
+  const informationList = document.getElementsByClassName(
+    'information-list',
+  )[0];
+  const moreInfo = document.getElementById('more-info');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+  let isDisableFlip = false;
+  let totalTurns = cardArray.length;
+  let totalTurnsRemaning = totalTurns;
 
-  function json(response) {
-    return response.json();
-  }
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
   function getRandomInts(num, max) {
-    var ints = [];
+    const ints = [];
     while (ints.length < num - 1) {
-      var randNum = getRandomInt(0, max);
+      const randNum = getRandomInt(0, max);
       if (!ints.indexOf(randNum) > -1) {
         ints.push(randNum);
       }
     }
     return ints;
   }
-  function init() {
-    refreshButton.addEventListener('click', refreshBoard);
-    restoreButton.addEventListener('click', resetBoard);
-    totalTurns = cardArray.length;
-    totalTurnsRemaning = totalTurns;
+
+  function resetFlipped(cards = []) {
+    for (let i = 0; i < cards.length; i += 1) {
+      const card = cards[i];
+      card.classList.remove(isFlippedClassname);
+    }
   }
-  const grid = document.querySelector('.grid');
-  const resultDisplay = document.querySelector('#result');
-  const remainingDisplay = document.querySelector('#remaining');
-  const container = document.querySelector('.container');
-  const refreshButton = document.getElementById('refresh-container');
-  const restoreButton = document.getElementById('restore-container');
-  const informationList = document.getElementsByClassName('information-list')[0];
-  const moreInfo = document.getElementById('more-info');
-  var cardsChosen = [];
-  var cardsChosenId = [];
-  var cardsWon = [];
-  var isDisableFlip = false;
-  var totalTurns = cardArray.length;
-  var totalTurnsRemaning = totalTurns;
+
+
+  function resetImages(backImages = []) {
+    for (let i = 0; i < backImages.length; i += 1) {
+      const backImage = backImages[i];
+      backImage.setAttribute('src', blankImagePath);
+    }
+  }
+
 
   function resultDisplayContent() {
     resultDisplay.textContent = cardsWon.length;
@@ -65,169 +68,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   function shuffle() {
-    for (let i = 0; i < 3 + (10 * Math.random()); i++) {
+    for (let i = 0; i < 3 + 10 * Math.random(); i += 1) {
       sortingRandom();
     }
   }
 
-  function createBoard() {
-
-    resultDisplayContent();
-    remainingDisplayContent();
-    shuffle();
-    for (let i = 0; i < cardArray.length; i++) {
-      var scene = document.createElement('div');
-      scene.classList.add('scene');
-
-      var card = document.createElement('div');
-      card.classList.add('card');
-      var front = document.createElement('div');
-      front.classList.add('card__face', 'card__face--front');
-      var frontImg = document.createElement('img');
-      frontImg.setAttribute('src', 'images/blank.png');
-      front.appendChild(frontImg);
-      front.setAttribute('data-id', i);
-      front.addEventListener('click', flipCard);
-      var back = document.createElement('div');
-      back.classList.add('card__face', 'card__face--back');
-      var backImg = document.createElement('img');
-      backImg.setAttribute('src', 'images/blank.png');
-      back.appendChild(backImg);
-      card.appendChild(front);
-      card.appendChild(back);
-      scene.appendChild(card);
-      grid.appendChild(scene);
-    }
-    moreInfo.addEventListener('click', moreInfoClick);
-  }
-
-  function resetFlipped(cards = []) {
-    for (let i = 0; i < cards.length; i++) {
-      const card = cards[i];
-      card.classList.remove('is-flipped');
-    }
-  }
-
-  function resetListeners(faceFronts = []) {
-    for (let i = 0; i < faceFronts.length; i++) {
-      const faceFront = faceFronts[i];
-      faceFront.removeEventListener('click', flipCard);
-      faceFront.addEventListener('click', flipCard);
-    }
-  }
-
-  function resetImages(backImages = []) {
-    for (let i = 0; i < backImages.length; i++) {
-      const backImage = backImages[i];
-      backImage.setAttribute('src', 'images/blank.png');
-    }
-  }
   function setFlipped(cards = []) {
-    for (let i = 0; i < cards.length; i++) {
+    for (let i = 0; i < cards.length; i += 1) {
       const card = cards[i];
-      card.classList.add('is-flipped');
+      card.classList.add(isFlippedClassname);
     }
   }
 
-  function refreshBoard() {
-    changeData(1);
-    deleteInformationList();
-    createInformationList();
-  }
-
-  function resetBoard() {
-    const cards = document.querySelectorAll('.card');
-    resetFlipped(cards);
-    const faceFronts = document.querySelectorAll('.card .card__face--front');
-    resetListeners(faceFronts);
-    const backImages = document.querySelectorAll('.card .card__face--front img');
-    resetImages(backImages);
-    shuffle();
-    cardsChosen = [];
-    cardsChosenId = [];
-    cardsWon = [];
-    isDisableFlip = false;
-    totalTurnsRemaning = totalTurns;
-    resultDisplayContent();
-    remainingDisplayContent();
-  }
-
-  function checkMatches() {
-    var cards = document.querySelectorAll('.card');
-    const optionOneId = cardsChosenId[0];
-    const optionTwoId = cardsChosenId[1];
-    const card1 = cards[optionOneId];
-    const card2 = cards[optionTwoId];
-    if (cardsChosen[0] === cardsChosen[1]) {
-      const front1 = card1.querySelector('.card__face--front');
-      const front2 = card2.querySelector('.card__face--front');
-      front1.removeEventListener('click', flipCard);
-      front2.removeEventListener('click', flipCard);
-      cardsWon.push(cardsChosen);
-      isDisableFlip = false;
-      resultDisplayContent();
-      if (cardsWon.length === cardArray.length / 2) {
-        resultDisplay.textContent = 'Congratulations you won game';
-      }
-      remainingDisplayContent();
-      totalTurnsRemaning = totalTurnsRemaning - 1;
-      if (totalTurnsRemaning === 0) {
-        setFlipped();
-      }
-    } else {
-      setTimeout(() => {
-        card1.classList.remove('is-flipped');
-        card2.classList.remove('is-flipped');
-        const imgBack1 = card1.querySelector('.card__face--back img');
-        imgBack1.setAttribute('src', 'images/blank.png');
-        const imgBack2 = card2.querySelector('.card__face--back img');
-        imgBack2.setAttribute('src', 'images/blank.png');
-        isDisableFlip = false;
-        resultDisplayContent();
-        if (cardsWon.length === cardArray.length / 2) {
-          resultDisplay.textContent = 'Congratulations you won game';
-        }
-        remainingDisplayContent();
-        totalTurnsRemaning = totalTurnsRemaning - 1;
-        if (totalTurnsRemaning === 0) {
-          alert('You lose');
-          setFlipped();
-        }
-      }, 1000);
+  const changeImageInformationList = () => {
+    const images = document.getElementsByClassName('information-image');
+    for (let i = 0; i < images.length; i += 1) {
+      const image = images[i];
+      const animal = animalArray[i];
+      image.setAttribute('src', animal.img);
+      image.setAttribute('alt', animal.name);
     }
-    cardsChosen = [];
-    cardsChosenId = [];
-  }
-
-  function flipCard() {
-    if (totalTurnsRemaning === 0) {
-      alert('You lose');
-      setFlipped();
-    } else {
-      if (!isDisableFlip) {
-        var cardId = this.getAttribute('data-id');
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-        const img = this.parentNode.querySelector('.card__face--back img');
-        img.setAttribute('src', cardArray[cardId].img);
-        this.parentNode.classList.add('is-flipped');
-        if (cardsChosen.length === 2) {
-          isDisableFlip = true;
-          checkMatches();
-        }
-      }
-    }
-
-  }
-
-  function deleteInformationList() {
-    for (let i = 0; i < informationList.length; i++) {
-      informationList.removeChild(list.childNodes[0]);
-    }
-  }
+  };
 
   function createInformationList() {
-    for (let i = 0; i < animalArray.length; i++) {
+    for (let i = 0; i < animalArray.length; i += 1) {
       const animal = animalArray[i];
       const listItem = document.createElement('li');
       const imageContainer = document.createElement('div');
@@ -248,41 +112,187 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   }
 
+  function flipCard() {
+    if (totalTurnsRemaning === 0) {
+      alert('You lose');
+      setFlipped();
+    } else if (!isDisableFlip) {
+      const cardId = this.getAttribute('data-id');
+      cardsChosen.push(cardArray[cardId].name);
+      cardsChosenId.push(cardId);
+      const img = this.parentNode.querySelector('.card__face--back img');
+      img.setAttribute('src', cardArray[cardId].img);
+      this.parentNode.classList.add(isFlippedClassname);
+      if (cardsChosen.length === 2) {
+        isDisableFlip = true;
+        // eslint-disable-next-line no-use-before-define
+        checkMatches();
+      }
+    }
+  }
+
+  function checkMatches() {
+    const cards = document.querySelectorAll('.card');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    const card1 = cards[optionOneId];
+    const card2 = cards[optionTwoId];
+    if (cardsChosen[0] === cardsChosen[1]) {
+      const front1 = card1.querySelector('.card__face--front');
+      const front2 = card2.querySelector('.card__face--front');
+      front1.removeEventListener('click', flipCard);
+      front2.removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
+      isDisableFlip = false;
+      resultDisplayContent();
+      if (cardsWon.length === cardArray.length / 2) {
+        resultDisplay.textContent = 'Congratulations you won game';
+      }
+      remainingDisplayContent();
+      totalTurnsRemaning -= 1;
+      if (totalTurnsRemaning === 0) {
+        setFlipped();
+      }
+    } else {
+      setTimeout(() => {
+        card1.classList.remove(isFlippedClassname);
+        card2.classList.remove(isFlippedClassname);
+        const imgBack1 = card1.querySelector('.card__face--back img');
+        imgBack1.setAttribute('src', blankImagePath);
+        const imgBack2 = card2.querySelector('.card__face--back img');
+        imgBack2.setAttribute('src', blankImagePath);
+        isDisableFlip = false;
+        resultDisplayContent();
+        if (cardsWon.length === cardArray.length / 2) {
+          resultDisplay.textContent = 'Congratulations you won game';
+        }
+        remainingDisplayContent();
+        totalTurnsRemaning -= 1;
+        if (totalTurnsRemaning === 0) {
+          alert('You lose');
+          setFlipped();
+        }
+      }, 1000);
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+  }
+
+  function createBoard() {
+    resultDisplayContent();
+    remainingDisplayContent();
+    shuffle();
+    for (let i = 0; i < cardArray.length; i += 1) {
+      const scene = document.createElement('div');
+      scene.classList.add('scene');
+      const card = document.createElement('div');
+      card.classList.add('card');
+      const front = document.createElement('div');
+      front.classList.add('card__face', 'card__face--front');
+      const frontImg = document.createElement('img');
+      frontImg.setAttribute('src', blankImagePath);
+      front.appendChild(frontImg);
+      front.setAttribute('data-id', i);
+      front.addEventListener('click', flipCard);
+      const back = document.createElement('div');
+      back.classList.add('card__face', 'card__face--back');
+      const backImg = document.createElement('img');
+      backImg.setAttribute('src', blankImagePath);
+      back.appendChild(backImg);
+      card.appendChild(front);
+      card.appendChild(back);
+      scene.appendChild(card);
+      grid.appendChild(scene);
+    }
+    moreInfo.addEventListener('click', moreInfoClick);
+  }
+
+
+  function resetListeners(faceFronts = []) {
+    for (let i = 0; i < faceFronts.length; i += 1) {
+      const faceFront = faceFronts[i];
+      faceFront.removeEventListener('click', flipCard);
+      faceFront.addEventListener('click', flipCard);
+    }
+  }
+
+  function resetBoard() {
+    const cards = document.querySelectorAll('.card');
+    resetFlipped(cards);
+    const faceFronts = document.querySelectorAll('.card .card__face--front');
+    resetListeners(faceFronts);
+    const backImages = document.querySelectorAll(
+      '.card .card__face--front img',
+    );
+    resetImages(backImages);
+    shuffle();
+    cardsChosen = [];
+    cardsChosenId = [];
+    cardsWon = [];
+    animalArray = [];
+    isDisableFlip = false;
+    totalTurnsRemaning = totalTurns;
+    resultDisplayContent();
+    remainingDisplayContent();
+  }
+  const status = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response);
+    }
+    return Promise.reject(new Error(response.statusText));
+  };
+
+  const json = (response) => response.json();
+
   function changeData(type = 0) {
-    fetch('./animal.json', { mode: 'cors' })
-      .then(status)
-      .then(json)
-      .then(function (data) {
-        console.log(data);
+    const handleData = (data) => {
+      const init = () => {
+        refreshButton.addEventListener('click', changeData);
+        restoreButton.addEventListener('click', resetBoard);
+        totalTurns = cardArray.length;
+        totalTurnsRemaning = totalTurns;
+      };
+      const addToArray = () => {
         animalData = data;
-        const randl = animalData["RandL"];
+        const randl = animalData.RandL;
         const animalNames = randl.items;
         const metaimg = randl.meta.img;
-        const prefix = metaimg.prefix;
-        const suffix = metaimg.suffix;
+        const { prefix } = metaimg;
+        const { suffix } = metaimg;
         const ints = getRandomInts(7, animalNames.length);
-        for (let i = 0; i < ints.length; i++) {
+        for (let i = 0; i < ints.length; i += 1) {
           const name = animalNames[ints[i]];
           const element = {
-            name: name,
-            img: `${randomAnimalImagesSite}${prefix}${name.replace(' ', '_')}${suffix}`
-          }
+            name,
+            img: `${randomAnimalImagesSite}${prefix}${name.replace(
+              /[\s|-]/,
+              '_',
+            )}${suffix}`,
+          };
           cardArray.push(element);
           cardArray.push(element);
           animalArray.push(element);
         }
-        if (type === 0) {
-          init();
-          createBoard();
-          createInformationList();
-        } else {
-          resetBoard();
-          deleteInformationList();
-          createInformationList();
-        }
-      }).catch(function (error) {
+      };
+      if (type === 0) {
+        init();
+        addToArray();
+        createBoard();
+        createInformationList();
+      } else {
+        resetBoard();
+        addToArray();
+        changeImageInformationList();
+      }
+    };
+    fetch('./animal.json', { mode: 'cors' })
+      .then(status)
+      .then(json)
+      .then((data) => handleData(data))
+      .catch((error) => {
         console.log('Request failed', error);
       });
   }
+
   changeData();
 });
